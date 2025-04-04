b1e7fba14c3611c0e0d18205123a335276bd23ff
Alex Sander <alex.darienko@gmail.com>
2025-04-04 21:20:51 +0300

    Homework done

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,66 @@
-const key = new Key();
+// Ключ
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+// Людина
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+// Абстрактний клас Дім
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person entered the house');
+    } else {
+      console.log('Door is closed');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+// Мій дім
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door is now open');
+    } else {
+      console.log('Wrong key!');
+    }
+  }
+}
+
+// Тестовий сценарій
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 
